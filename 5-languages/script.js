020e4ab8c77c2af1d558f47b1f9777c1f6076197
Konstantin <konkomen1@gmail.com>
2023-11-26 10:08:06 +0300

    Задание переводчик

---
+++ 5-languages/script.js
@@ -0,0 +1,19 @@
+const userLanguage = prompt('Какой ваш язык?')
+
+switch (userLanguage) {
+  case 'russian':
+    console.log('Здравствуйте');
+    break;
+  case 'german':
+    console.log('Guten Tag');
+    break;
+  case 'english':
+    console.log('Hello');
+    break;
+  case 'norwegian':
+    console.log('Hei');
+    break;
+  default:
+    console.log('Hello everyone!=)');
+    break;
+}
